b1f37949b74535c60264b057af4fc62928299a85
Eric Neubauer <neubee64@gmail.com>
2022-08-05 13:41:04 +0800

    experiment with manual file moving

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,16 @@
-import adapter from "@sveltejs/adapter-cloudflare";
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
-    // adapter: adapter({
-    //   pages: "build",
-    //   assets: "build",
-    //   fallback: "index.html",
-    //   precompress: false,
-    // }),
+    // adapter: adapter(),
+    adapter: adapter({
+      pages: "build",
+      assets: "build",
+      // fallback: null,
+      fallback: "index.html",
+      precompress: false,
+    }),
 
     // prerender: {
     // 	// This can be false if you're using a fallback (i.e. SPA mode)
