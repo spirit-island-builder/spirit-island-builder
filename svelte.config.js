b42518179b7e9b7e82c50e028a4e88dfb2a5c8b5
Eric Neubauer <neubee64@gmail.com>
2022-08-05 13:32:38 +0800

    test cloudflare again

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,15 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-cloudflare";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      pages: "build",
-      assets: "build",
-      fallback: "index.html",
-      precompress: false,
-    }),
+    adapter: adapter(),
+    // adapter: adapter({
+    //   pages: "build",
+    //   assets: "build",
+    //   fallback: "index.html",
+    //   precompress: false,
+    // }),
 
     // prerender: {
     // 	// This can be false if you're using a fallback (i.e. SPA mode)
