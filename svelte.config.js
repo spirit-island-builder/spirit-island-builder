7e716d89576a86622137c53e8fbaeb2de569150a
Eric Neubauer <neubee64@gmail.com>
2022-08-05 13:11:47 +0800

    try SPA via static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,19 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      pages: "build",
+      assets: "build",
+      fallback: "test.html",
+      precompress: false,
+    }),
+
+    // prerender: {
+    // 	// This can be false if you're using a fallback (i.e. SPA mode)
+    // 	default: true
+    // }
   },
 };
 
