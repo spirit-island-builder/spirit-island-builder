241670433e41c4ecfb2c20f888c1736f0996fda2
Eric Neubauer <neubee64@gmail.com>
2022-08-05 11:23:52 +0800

    Go back to auto adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,9 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-auto";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter(),
-    prerender: {
-      // This can be false if you're using a fallback (i.e. SPA mode)
-      default: true,
-    },
   },
 };
 
