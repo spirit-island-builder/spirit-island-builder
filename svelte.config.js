fa89388f49b6c4f5daec161ca0bbf342551ae5ef
Eric Neubauer <neubee64@gmail.com>
2022-08-05 10:47:49 +0800

    Try static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,14 @@
-import adapter from "@sveltejs/adapter-cloudflare";
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
-	},
+  kit: {
+    adapter: adapter(),
+  },
+  prerender: {
+    // This can be false if you're using a fallback (i.e. SPA mode)
+    default: true,
+  },
 };
 
 export default config;
