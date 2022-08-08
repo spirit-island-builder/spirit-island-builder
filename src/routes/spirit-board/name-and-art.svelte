34b5595b6651ac8a317e7c7461871487dfc8434a
Eric Neubauer <neubee64@gmail.com>
2022-08-08 14:49:57 +0800

    Example handling of file inputs

--- src/routes/spirit-board/name-and-art.svelte
+++ src/routes/spirit-board/name-and-art.svelte
@@ -1,4 +1,28 @@
 <script>
+  function handleImageFileInput(event) {
+    const file = event.target.files.item(0);
+    if (file) {
+      const fileReader = new FileReader();
+      fileReader.onload = (data) => {
+        const imageURL = data.target.result;
+
+        const targetId = event.target.id;
+        if (targetId.toLowerCase().includes("spiritart")) {
+          spiritBoard.nameAndArt.artPath = imageURL;
+        } else if (targetId.toLowerCase().includes("spiritbanner")) {
+          spiritBoard.nameAndArt.bannerPath = imageURL;
+        } else if (targetId.toLowerCase().includes("energybanner")) {
+          spiritBoard.nameAndArt.energyBannerPath = imageURL;
+        } else if (targetId.toLowerCase().includes("playsbanner")) {
+          spiritBoard.nameAndArt.playsBannerPath = imageURL;
+        }
+      };
+
+      // This reads the file and then triggers the onload function above once it finishes
+      fileReader.readAsDataURL(file);
+    }
+  }
+
   // exports allow for properties to be passed into this component. So the value of spiritBoard can be set by whatever component is the parent of this one. See https://svelte.dev/tutorial/declaring-props
   export let spiritBoard;
   export let showOrHideSection;
@@ -34,15 +58,26 @@
       <label class="label" for="spiritArtInput">Spirit Art</label>
     </div>
     <div class="control">
+      <!-- Can use CSS to change how this looks. Maybe we could use a toggle to switch between file input and URL input -->
+      <input
+        accept="image/png, image/jpeg"
+        on:change={handleImageFileInput}
+        id="spiritArtFileInput"
+        name="spiritArtFileInput"
+        type="file"
+        class="input" />
+      <!-- Showing that the image is available -->
+      {#if spiritBoard.nameAndArt.artPath}
+        <img id="spiritArtInputImage" src={spiritBoard.nameAndArt.artPath} alt="spirit art" />
+      {/if}
       <input
         id="spiritArtInput"
         class="input is-small"
         type="text"
-        disabled
         placeholder="File Name"
         bind:value={spiritBoard.nameAndArt.artPath} />
+      <!-- No need for a button because spiritBoard.nameAndArt.artPath is bound to this input already, and the image won't be loaded until the board gets generated again -->
     </div>
-    <button class="button is-warning is-light is-small row-button">Load File</button>
   </div>
   <div class="field has-addons is-horizontal is-justify-content-left">
     <div class="field-label is-small">
