7340502b3caea10645bbb5b3c3d58d41b0aceaae
Tom Prince <tom.prince@hocat.ca>
2023-01-17 02:01:36 -0700

    Use `<LoadButton>` for custom icons as well.

--- src/lib/load-button.svelte
+++ src/lib/load-button.svelte
@@ -3,6 +3,7 @@
   export { classList as class };
   export let accept;
   export let loadObjectURL;
+  export let loadDataURL;
 
   let fileInput;
   let files;
@@ -10,10 +11,21 @@
   const handleInput = () => {
     const file = files.item(0);
     if (file) {
-      let url = URL.createObjectURL(file);
-      Promise.resolve(loadObjectURL(url)).finally(() => {
-        URL.revokeObjectURL(url);
-      });
+      if (loadObjectURL) {
+        let url = URL.createObjectURL(file);
+        Promise.resolve(loadObjectURL(url)).finally(() => {
+          URL.revokeObjectURL(url);
+        });
+      }
+      if (loadDataURL) {
+        const fileReader = new FileReader();
+        fileReader.onload = (event) => {
+          loadDataURL(event.target.result);
+        };
+
+        // This reads the file and then triggers the onload function above once it finishes
+        fileReader.readAsDataURL(file);
+      }
     }
   };
 </script>
