28984e41d824aefb93f43a27ba7f88d62a935981
Tom Prince <tom.prince@hocat.ca>
2023-02-15 13:07:13 -0700

    Move page footer to a separate component.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,7 @@
   import Aspect from "./aspect/index.svelte";
   import Adversary from "./adversary/index.svelte";
   import Instructions from "$lib/instructions/index.svelte";
+  import Footer from "./footer.svelte";
 
   import { divertDownload, downloadData } from "$lib/download";
 
@@ -427,31 +428,7 @@
     {/await}
   {/if}
 
-  <article class="message is-small mb-1">
-    <div class="message-body p-1">
-      See <a href="https://neubee.github.io/spirit-island-builder/instructions" target="_blank"
-        >Instructions</a>
-      for details on how to use the form. For custom art,
-      <a href="https://www.wombo.art/" target="_blank">Wombo</a>
-      (unaffiliated) is a popular art generator.
-      <span class="is-pulled-right"
-        >Code for this project is hosted <a
-          href="https://github.com/neubee/spirit-island-builder"
-          target="_blank">here</a
-        >{#if import.meta.env.VITE_COMMIT_SHA !== undefined}
-          &nbsp;(built from
-          <a
-            href="https://github.com/neubee/spirit-island-builder/commit/{import.meta.env
-              .VITE_COMMIT_SHA}"
-            target="_blank"
-            rel="noreferrer">{import.meta.env.VITE_COMMIT_SHA.substring(0, 8)}</a
-          >){/if}.</span>
-      <br />This is an unofficial website. Interface created by Neubee & Resonant. The Spirit Island
-      Builder is adapted from
-      <a href="https://github.com/Gudradain/spirit-island-template" target="_blank">HTML template</a>
-      developed by Spirit Island fanbase. All materials belong to Greater Than Games, LLC.
-    </div>
-  </article>
+  <Footer />
 </div>
 
 <style>
---
+++ src/routes/footer.svelte
@@ -0,0 +1,25 @@
+<footer class="message is-small mb-1">
+  <div class="message-body p-1">
+    See <a href="https://neubee.github.io/spirit-island-builder/instructions" target="_blank"
+      >Instructions</a>
+    for details on how to use the form. For custom art,
+    <a href="https://www.wombo.art/" target="_blank">Wombo</a>
+    (unaffiliated) is a popular art generator.
+    <span class="is-pulled-right"
+      >Code for this project is hosted <a
+        href="https://github.com/neubee/spirit-island-builder"
+        target="_blank">here</a
+      >{#if import.meta.env.VITE_COMMIT_SHA !== undefined}
+        &nbsp;(built from
+        <a
+          href="https://github.com/neubee/spirit-island-builder/commit/{import.meta.env
+            .VITE_COMMIT_SHA}"
+          target="_blank"
+          rel="noreferrer">{import.meta.env.VITE_COMMIT_SHA.substring(0, 8)}</a
+        >){/if}.</span>
+    <br />This is an unofficial website. Interface created by Neubee & Resonant. The Spirit Island
+    Builder is adapted from
+    <a href="https://github.com/Gudradain/spirit-island-template" target="_blank">HTML template</a>
+    developed by Spirit Island fanbase. All materials belong to Greater Than Games, LLC.
+  </div>
+</footer>
