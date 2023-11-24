1e09ee8aaa2928c925d337d69a1886a54f5767e8
resonant-gamedesign <44596045+resonant-gamedesign@users.noreply.github.com>
2023-11-23 22:52:07 -0600

    fixes for eslint/prettier

--- src/lib/preview-frame/take-screenshot.js
+++ src/lib/preview-frame/take-screenshot.js
@@ -2,16 +2,14 @@
 // preview frame. It exports `takeScreenshot` by adding it as an
 // attribute on window, which can be accessed from the main frame.
 
-
-
 /* in ES 6 */
-import domtoimage from 'dom-to-image-more';
+import domtoimage from "dom-to-image-more";
 
 window.takeScreenshot = async (elementName, scale = 1) => {
   let element = document.querySelector(elementName);
   console.log("scale=" + scale);
-  let height = element.offsetHeight ;
-  let width = element.offsetWidth ;
+  let height = element.offsetHeight;
+  let width = element.offsetWidth;
   console.log("Height: " + height);
   console.log("width: " + width);
   let canvas = await domtoimage.toCanvas(element, {
@@ -19,9 +17,7 @@ window.takeScreenshot = async (elementName, scale = 1) => {
     backgroundColor: null,
     height: height,
     width: width,
-    scale: scale
+    scale: scale,
   });
   return canvas.toDataURL();
 };
-
-
