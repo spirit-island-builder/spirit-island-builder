44e105af64c94194049240be73219f12f3eb5c0d
Eric Neubauer <neubee64@gmail.com>
2026-01-19 11:20:07 -0500

    Add link in footer. Convert privacy policy code to more svelte like instead of generic html.

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -4,7 +4,7 @@
     target="_blank"
     rel="noreferrer">this form</a>
   or email <a href="mailto:resonant@spiritislandbuilder.com">resonant@spiritislandbuilder.com</a>
-  with questions, comments or feedback.
+  with questions, comments or feedback. <a href="/privacypolicy">Privacy Policy</a>
   <span class="is-pulled-right has-text-right"
     >Code for this project is hosted <a
       href="https://github.com/neubee/spirit-island-builder"
